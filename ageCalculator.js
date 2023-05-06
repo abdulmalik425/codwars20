5120a000b6947d6a56ed1e4cb043bde739b69e0f
Abdulmalik425 <malik.salah98@gmail.com>
2023-05-06 09:03:23 -0500

    Calculates age based on year born, pretty cool one

---
+++ ageCalculator.js
@@ -0,0 +1,21 @@
+/*
+Determine what age you were if 
+its year Y and you were born in year X
+*/
+
+function  calculateAge(birthYear,year) {
+    age = year-birthYear;
+    if(age==1)
+      return "You are 1 year old.";
+    else if(age===-1)
+      return "You will be born in 1 year."
+    else if(age>0)
+      return "You are " + age + " years old.";
+    else if(age<0) {
+      age = birthYear-year;
+      return  "You will be born in " + age + " years."
+    }
+    else {
+      return "You were born this very year!";
+    }
+ }
