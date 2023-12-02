86a18c1dc1cfe87904d063f0c6a656bc0114cdd3
Abdulmalik425 <malik.salah98@gmail.com>
2023-12-02 01:24:21 -0600

    Testing

---
+++ reverseList.js
@@ -0,0 +1,18 @@
+/*
+In this kata you will create a function 
+that takes in a list and returns a list with 
+the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+P nums
+R smun
+E 1,2,23,3 3,23,2,1
+P for each, split, reverse
+*/
+
+function reverseList(list) {
+    return list.reverse();
+  }
+
+
