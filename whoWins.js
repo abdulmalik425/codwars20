6fa30a25beec8f11d981b0b23eec86c4c8e73f4c
Abdulmalik425 <malik.salah98@gmail.com>
2023-12-29 01:18:05 -0600

    who wins challenge

---
+++ whoWins.js
@@ -0,0 +1,17 @@
+/*
+If the winner is George Saint Pierre he will obviously say:
+
+"I am not impressed by your performance."
+If the winner is Conor McGregor he will most undoubtedly say:
+
+"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+
+*/
+
+var quote = function(fighter) {
+    switch(fighter.toLowerCase()) {
+      case 'conor mcgregor': return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+      case 'george saint pierre': return "I am not impressed by your performance.";
+      default : 'Who are you?' 
+    }
+  };
